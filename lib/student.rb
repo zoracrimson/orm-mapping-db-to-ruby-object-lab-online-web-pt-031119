1e49768b5623c98f3e90dde6b8e37c18255d074d
Kat Maldon <katharinamaldon@gmail.com>
2019-05-24 02:07:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     end.first
   end
 
-  def self.count_all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 9
